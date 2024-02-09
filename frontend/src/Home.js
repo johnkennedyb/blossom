625b8947a069f8bfcd3d1d519e075bba11e4b33a
unknown <johnkennedy3313@gmail.com>
2024-02-09 17:56:28 +0100

    Initial commit

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="container-fluid p-0 mb-5">
             <div className="row">
                 <div className="col-md-12-lg-6 position-relative bg-dark text-white py-5" style={{backgroundImage: `url('https://images.pexels.com/photos/7356/startup-photos-7356.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`, backgroundSize: 'cover', backgroundPosition: 'center' ,zIndex: 1}}>
-                    <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50" style={{ zIndex: -1 }}></div>
+                    <div className="position-absolute top-0 start-0 w-100 h-100  opacity-50" style={{ zIndex: -1, backgroundColor: "#6a0dad" } }></div>
                     <div className="text-left py-5 px-5 " >
                         <h1 className='text-white pt-5 pb-3' style={{ zIndex: 100 }}>Business Planning and Execution</h1>
                         <p className="lead ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt<br/> ut labore et dolore magna aliqua.
@@ -42,6 +42,107 @@ const Home = () => {
                 </div>
                 
             </div>
+            <div class=" text-white py-5 bg-purple" style={{ backgroundColor: "#6a0dad" }}>
+    <div class="container">
+        <div class="row justify-content-center py-5">
+            <div class="col-md-6 text-start">
+                <h2 class="text-left text-white  pb-2  text-start">About Us</h2>
+                <p class="text-left text-white  text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et suscipit neque. Mauris sed nunc ac lorem feugiat placerat. Proin ut bibendum felis. Quisque vel urna a arcu posuere vestibulum non et turpis. Duis nec tortor quam. Suspendisse nec faucibus felis. Nullam eget mi a nunc placerat molestie eget nec lacus. Pellentesque quis eros sed dolor pretium finibus. Donec ultricies, nunc non fermentum molestie, ipsum odio hendrerit elit, id dignissim mi elit vitae lorem.</p>
+                <p class="text-left text-white  pb-2 text-start">Phasellus rutrum arcu nec ex fermentum, nec venenatis leo accumsan. Phasellus non odio dolor. Duis vitae posuere metus. Duis auctor, elit sit amet tempus fermentum, purus orci convallis purus, in placerat turpis lectus in ligula. Fusce vel gravida libero. Duis maximus malesuada nibh, in eleifend purus finibus eget. Pellentesque vitae dolor lectus. Cras venenatis turpis in orci vehicula aliquet.</p>
+                <button type="button" class="btn btn-secondary p-3 text-start btn-start">Our Service</button>
+            </div>
+            <div class="col-md-6">
+                <img src="https://images.pexels.com/photos/1181533/pexels-photo-1181533.jpeg?auto=compress&cs=tinysrgb&w=600" alt="About" class="img-fluid" />
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<div class="bg-light text-dark py-5">
+
+<div className="container my-5 bg-light">
+<h1 className='text-start my-5'>Services</h1>
+
+    <div className="row">
+        <div className="col-md-4">
+            <div className="card">
+                <img src="https://images.pexels.com/photos/2672979/pexels-photo-2672979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="Service 1" />
+                <div className="card-body">
+                    <h5 className="card-title">Economy</h5>
+                    <p className="card-text">Phasellus rutrum arcu nec ex fermentum, nec venenatis leo accumsan. Phasellus non odio dolor. Duis vitae posuere metus</p>
+                    <a href="#" className="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-4">
+            <div className="card">
+                <img src="https://images.pexels.com/photos/1181264/pexels-photo-1181264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="Service 2" />
+                <div className="card-body">
+                    <h5 className="card-title">Reflecting</h5>
+                    <p className="card-text">Phasellus rutrum arcu nec ex fermentum, nec venenatis leo accumsan. Phasellus non odio dolor. Duis vitae posuere metus</p>
+                    <a href="#" className="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-4">
+            <div className="card">
+                <img src="https://images.pexels.com/photos/380769/pexels-photo-380769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="Service 3" />
+                <div className="card-body">
+                    <h5 className="card-title">Standard</h5>
+                    <p className="card-text">Phasellus rutrum arcu nec ex fermentum, nec venenatis leo accumsan. Phasellus non odio dolor. Duis vitae posuere metus</p>
+                    <a href="#" className="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<img
+  src="https://images.pexels.com/photos/1181533/pexels-photo-1181533.jpeg?auto=compress&cs=tinysrgb&w=600"
+  alt="About"
+  className="img-fluid w-100"
+  style={{
+    height: '400px',
+    backgroundAttachment: 'fixed',
+    objectFit: 'cover',
+    position: 'fixed',
+
+  }}
+/>
+
+
+<div className="text-white py-5 bg-purple" style={{ background: "linear-gradient(to bottom, #6a0dad, white)" }}>
+
+
+<div class="row justify-content-center py-5 ">
+    <div class="col-md-6 p-5 text-start">
+        <h2 class="text-left text-white  pb-2  text-start">About Us</h2>
+        <p class="text-left text-white  text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et suscipit neque. Mauris sed nunc ac lorem feugiat placerat. Proin ut bibendum felis. Quisque vel urna a arcu posuere vestibulum non et turpis. Duis nec tortor quam. Suspendisse nec faucibus felis. Nullam eget mi a nunc placerat molestie eget nec lacus. Pellentesque quis eros sed dolor pretium finibus. Donec ultricies, nunc non fermentum molestie, ipsum odio hendrerit elit, id dignissim mi elit vitae lorem.</p>
+        <button type="button" class="btn btn-secondary p-3 text-start btn-start">Our Service</button>
+    </div>
+    <div class="col-md-6">
+        <img src="https://images.pexels.com/photos/1181533/pexels-photo-1181533.jpeg?auto=compress&cs=tinysrgb&w=600" alt="About" class="img-fluid" />
+    </div>
+</div>
+
+<div class="row justify-content-center py-5 ">
+    <div class="col-md-6 px-5">
+        <img src="https://images.pexels.com/photos/380769/pexels-photo-380769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="About" class="img-fluid" />
+    </div>
+    <div class="col-md-6  text-start">
+        <h2 class="text-left text-white  pb-2  text-start">Lorem ipsum dolor sit amet</h2>
+        <p class="text-left text-white  text-start"> Sed et suscipit neque. Mauris sed nunc ac lorem feugiat placerat. Proin ut bibendum felis. Quisque vel urna a arcu posuere vestibulum non et turpis. Duis nec tortor quam. Suspendisse nec faucibus felis. Nullam eget mi a nunc placerat molestie eget nec lacus. Pellentesque quis eros sed dolor pretium finibus. Donec ultricies, nunc non fermentum molestie, ipsum odio hendrerit elit, id dignissim mi elit vitae lorem.</p>
+        <button type="button" class="btn btn-secondary p-3 text-start btn-start">Our Service</button>
+    </div>
+</div>
+</div>
         </div>
     );
 }
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
+<nav className="navbar navbar-expand-lg navbar-light bg-transparent" style={{ backgroundColor: "rgba(255, 255, 255, 0.5)", marginBottom: "-50px" , zIndex: "20" }}>
       <div className="container mx-5 ">
         <a className="navbar-brand text-white" href="/">MERN Stack Website</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,8 +10,8 @@ const Navbar = () => {
         </button>
         
       </div>
-      <div className="collapse navbar-collapse text-white mx-5" id="navbarNav">
-          <ul className="navbar-nav ml-auto text-white">
+      <div className="collapse navbar-collapse text-dark mx-5" id="navbarNav">
+          <ul className="navbar-nav ml-auto text-dark">
             <li className="nav-item text-white">
               <a className="nav-link text-white" href="#">Home</a>
             </li>
